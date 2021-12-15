dfa1dfce7eca1cb6775eeabc2fbd966d34396755
Tashmax19 <tashma19@gmail.com>
2021-12-15 22:10:33 +0300

    update- added login form

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,8 @@
 import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.css'
+//import styles from '../styles/Login.modules.css'
+
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
