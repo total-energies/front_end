b19e4590e9af14f45845e747b1829dfef74f9e83
ugerpaul17 <ugerpaul5@gmail.com>
2022-01-19 09:22:42 +0300

    export button

--- components/Searchbar.js
+++ components/Searchbar.js
@@ -1,51 +1,21 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { Form } from "react-bootstrap";
 import styles from '../styles/Searchbar.module.css';
+import { Table } from "react-bootstrap";
 import axios from "axios";
+import Pdf from "react-to-pdf";
 
- import Table from '../components/SearchTable';
+ //import Table from '../components/SearchTable';
+
+const ref = React.createRef();
 
 
- 
-
-
-
- 
-
-function Searchbar ({data}) {
+function Searchbar () {
   const [show, setShow] = useState(false);
   const [disable, setSearchButton] = useState(false);
   const [searchResults, setSearchResults] = useState([]);
   const [formInput, setFormInputs] = useState({});
   const [searchTerm, setSearchTerm] = useState('');
-    // const [APIData, setAPIData] = useState([]);
-    // const [filteredResults, setFilteredResults] = useState([]);
-    // const [searchInput, setsearchInput] = useState('');
-
-  // const url = '';
-  //   useEffect(() => {
-  //     axios.get(url)
-  //     .then((response) => {
-  //       console.log(response);
-  //       setAPIData(response.data);
-        
-  //     })
-  //   }, [])
-
-
-  //   const searchItems = (searchValue) => {
-  //     setsearchInput(searchValue)
-  //     if(searchInput !== ''){
-  //       const filteredData = APIData.filter((item) => {
-  //         return Object.values(item).join('').toLowerCase().includes(searchInput.toLowerCase())
-  //       })
-  //       setFilteredResults(filteredData);
-  //     }
-  //     else {
-  //       setFilteredResults(APIData)
-  //     }
-  //   }
-
 
   const handleInputs = (event) => {
     setShow(false);
@@ -55,7 +25,6 @@ function Searchbar ({data}) {
     setSearchTerm(event.target.value);
   }
 
-
   const handleEnterKey =(event) => {
     if (event.key === 'Enter') {
       toggleButton();
@@ -63,7 +32,6 @@ function Searchbar ({data}) {
     }
 
   }
-
 
   const search = async (event) => {
     event.preventDefault();
@@ -84,15 +52,6 @@ function Searchbar ({data}) {
     console.log('in God we trust',searchedResults);
 
   } 
-
-
-
-
-
-    function toggle(){
-      setShow(!show);
-    }
-
     function toggleButton(){
       setSearchButton(true);
       setShow(true);
@@ -164,7 +123,9 @@ function Searchbar ({data}) {
        </div>
        
        <div className={styles.searchButton}>
-        <button type="button" name="exportBtn"  className={styles.buttonType} id={styles.exportButton} >Export</button>
+         <Pdf target={ref} filename="anwsers.pdf">
+          {({ toPdf }) => <button type="button" name="exportBtn"  className={styles.buttonType} id={styles.exportButton} onClick={toPdf}>Export</button>}
+        </Pdf>
        </div>
        </div>
 
@@ -172,8 +133,40 @@ function Searchbar ({data}) {
         display: show?"block":"none"
       }}>
 
+
+<Table responsive="xl" className="p-2 mt-5 TableData" ref={ref}>
+        <tbody>
+          <tr className="mt-2">
+           
+            <td> <input type="checkbox" className="form-check-input ms-1" id="exampleCheck1"/></td>
+            <td>
+            <span>Environment/Owener Pauline</span>
+              <p className="text-secondary">The Integrated Lake Albert Resource Development Project, including the Tilenga development and the EACOP pipeline, is a project consistent with this strategy of low-dead-end projects that are the most efficient in terms of greenhouse gas emissions. In particular, the greenhouse gas emissions intensity of this project is 13 kgCO2/boe, i.e. less than 1 MtCO2eq/year at the plateau (Scope 1 + 2), corresponding to a level much lower than the current average of TotalEnergies oil production which is of the order of 19 kgCO2/bep. </p>
+           </td>
+           
+          </tr>
+          
+          <tr className="mt-2">
+            <td><input type="checkbox" className="form-check-input ms-1" id="exampleCheck1"/></td>
+            <td>
+            <span>Financing/Owener Fabien</span>
+              <p className="text-secondary">The integrated project to develop the Lake Albert resources, which consists of the Tilenga development and the EACOP pipeline, is consistent with our strategy to focus on projects with both a low breakeven point and low greenhouse gas footprint. This projects GHG emissions intensity comes to 13 kg CO2/boe, which is well below the current average of our oil portfolio of around 20 kg/boe.</p>
+           </td>
+           
+          </tr>
+          <tr className="mt-2">
+            <td><input type="checkbox" className="form-check-input ms-1" id="exampleCheck1"/></td>
+            <td>
+            <span>Security/Owener Jean Michel</span>
+              <p className="text-secondary">To-date - 10,000 people in the Tilenga footprint area, have been held over 3 years, to explain the land acquisition process and ensure that all interested and affected parties are informed and involved throughout the process.</p>
+          </td>
+           
+          </tr>
+        </tbody>
+      </Table>
+
      
-        <Table data={searchResults} />
+        {/* <Table data={searchResults} /> */}
     
 
       
