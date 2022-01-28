87e857d6b9dcf20525aaa1572db3c6d222727a8e
Mercy-Wamanga <tashma19@gmail.com>
2022-01-28 09:23:39 +0300

    updates-post for sub themes and answers

--- components/Cards.js
+++ components/Cards.js
@@ -7,6 +7,7 @@ import icon from '../public/categories.png';
 import plus from '../public/add.png';
 import pieChart from '../public/pie-chart.png';
 import barChart from '../public/bar-graph.png';
+import data from '../db.json';
 
 
 
@@ -27,7 +28,6 @@ export default function Cards() {
       const response = await axios.post(ownerUrl,{id,...owner}).then(()=>{
         console.log(response);
       });
-   
   }
 
 
@@ -38,46 +38,56 @@ export default function Cards() {
       alert("Please Add Owner");
       return
     }
-
     addOwner({owner});
-
     setOwner('');
   }
 
 
   // theme and subtheme posting methods
   const themeUrl= 'http://localhost:3000/themes';
-  const subthemeUrl= 'http://localhost:3000/subthemes';
+ 
 
 
-  const addThemes= (theme,subtheme) =>{
+  const addThemes=async theme =>{
     //  console.log(theme);
     //  console.log(subtheme);
-    
     const id=Math.floor(Math.random()*1000)+1;
-
-    const reqOne = axios.post(themeUrl,{id,...theme});
-    const reqTwo = axios.post(subthemeUrl,{id,...subtheme});
-
-
-    axios.all([reqOne, reqTwo]).then(axios.spread((...responses) => {
-      const responseOne = responses[0]
-      const responseTwo = responses[1]
-    })).catch(errors => {
-      console.log(errors)
-    })
-    
+    const response = await axios.post(themeUrl,{id,...theme}).then(()=>{
+      console.log(response);
+    });    
  }
 
  const themeSubmit=(e) => {
   e.preventDefault();
 
-  if(!theme || !subtheme ){
+  if(!theme){
     alert("Please Add Theme and Subtheme");
     return
   }
 
-  addThemes({theme,subtheme});
+  addThemes({theme});
+  setTheme('');
+}
+
+const subthemeUrl= 'http://localhost:3000/subthemes';
+const addSubThemes=async (theme,subtheme) =>{
+  //  console.log(theme);
+  //  console.log(subtheme);
+  const id=Math.floor(Math.random()*1000)+1;
+  const response = await axios.post(subthemeUrl,{id,...theme,...subtheme}).then(()=>{
+    console.log(response);
+  });    
+}
+
+const subthemeSubmit=(e) => {
+  e.preventDefault();
+
+  if(!subtheme){
+    alert("Please Add Subtheme");
+    return
+  }
+
+  addSubThemes({theme,subtheme});
   setTheme('');
   setSubtheme('');
 }
@@ -105,7 +115,6 @@ export default function Cards() {
                 </div>
               </form>
             </div>
-
           </div>
         </div>
       </div>
@@ -122,6 +131,38 @@ export default function Cards() {
             <div className="modal-body form-center">
               <form  onSubmit={themeSubmit}>
                 <input type="text" className="form-control mt-3" value={theme} id="exampleFormControlInput1" placeholder="Theme" onChange={(e)=>setTheme(e.target.value)} ></input>
+                <div className="modal-footer">
+              <button type="submit" className={styles.saveButton}>Save Data</button>
+            </div>
+              </form>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+
+
+      
+      {/* Sub Theme Modal */}
+      <div className="modal fade " id="ModalSubTheme" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal-dialog ">
+          <div className="modal-content" id={styles.modalPosition}>
+            <div className="modal-header">
+              <h5 className="modal-title" id={styles.titleAlign}>Add Sub Theme</h5>
+              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div className="modal-body form-center">
+              <form  onSubmit={subthemeSubmit}>
+                {/* <input type="text" className="form-control mt-3" value={theme} id="exampleFormControlInput1" placeholder="Theme" onChange={(e)=>setTheme(e.target.value)} ></input> */}
+                <select defaultValue={'DEFAULT'} className="form-select mt-3" value={theme} onChange={(e)=>setTheme(e.target.value)} aria-label="Default select example">
+                <option value="DEFAULT">--Theme---</option>
+
+                {
+                  data.themes.map((result)=>(<option key={result.id}>{result.theme}</option>))
+                }
+              
+               
+              </select>
                 <input type="text" className="form-control mt-3" value={subtheme} id="exampleFormControlInput1" placeholder="Sub Theme" onChange={(e)=>setSubtheme(e.target.value)} ></input>
 
                 <div className="modal-footer">
@@ -135,7 +176,6 @@ export default function Cards() {
       </div>
       {/*-----modals----  */}
 
-
       <div className='container p-5 mt-5'>
         {/* <div className='row p-3'>
              <div className='col-md-12'>
@@ -147,8 +187,37 @@ export default function Cards() {
             <div className="card rounded-3 bg-primary" id={styles.cardOne}>
               <div className="card-body" id={styles.containerIconz}>
 
+              <Image
+                  src={pieChart}
+                  id={styles.icons}
+                  alt='category icon'
+                  width={100}
+                  height={100}
+                />
 
-                <Image
+                <div className="ms-3">
+                  <h5 className="card-text text-white mt-2">Owners</h5>
+                  <h1 className='text-white'>45</h1>
+                </div>
+
+                <button className="btn btn-default align-bottom">
+                  <Image onClick={() => setShow(true)}
+                    src={plus}
+                    alt='plus icon'
+                    width={50}
+                    height={50}
+                    data-bs-toggle="modal" data-bs-target="#ModalOwner"
+                  />
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div className='col-md-4 mt-3'>
+            <div className="card rounded-3" id={styles.cardTwo}>
+              <div className="card-body" id={styles.containerIconz}>
+
+              <Image
                   src={icon}
                   alt='category icon'
                   id={styles.icons}
@@ -178,38 +247,6 @@ export default function Cards() {
               </div>
             </div>
           </div>
-
-          <div className='col-md-4 mt-3'>
-            <div className="card rounded-3" id={styles.cardTwo}>
-              <div className="card-body" id={styles.containerIconz}>
-
-                <Image
-                  src={pieChart}
-                  id={styles.icons}
-                  alt='category icon'
-                  width={100}
-                  height={100}
-                />
-
-                <div className="ms-3">
-                  <h5 className="card-text text-white mt-2">Owners</h5>
-                  <h1 className='text-white'>45</h1>
-                </div>
-
-                <button className="btn btn-default align-bottom">
-                  <Image onClick={() => setShow(true)}
-                    src={plus}
-                    alt='plus icon'
-                    width={50}
-                    height={50}
-                    data-bs-toggle="modal" data-bs-target="#ModalOwner"
-                  />
-                </button>
-
-
-              </div>
-            </div>
-          </div>
           <div className='col-md-4 mt-3'>
 
             <div className="card rounded-3" id={styles.cardThree}>
@@ -225,9 +262,20 @@ export default function Cards() {
                 />
 
                 <div className='ms-3'>
-                  <h5 className="card-text text-white mt-2">Answers</h5>
+                  <h5 className="card-text text-white mt-2">SubThemes</h5>
                   <h1 className='text-white'>45</h1>
                 </div>
+                <button className="btn btn-default align-bottom">
+                  <Image onClick={() => setShow(true)}
+                    src={plus}
+                    alt='plus icon'
+
+                    id={styles.iconsPlus}
+                    width={50}
+                    height={50}
+                    data-bs-toggle="modal" data-bs-target="#ModalSubTheme"
+                  />
+                </button>
 
               </div>
             </div>
--- components/Searchbar.js
+++ components/Searchbar.js
@@ -2,107 +2,141 @@ import React, { useEffect, useState } from "react";
 import { Form } from "react-bootstrap";
 import styles from '../styles/Searchbar.module.css';
 import axios from "axios";
+import data from '../db.json';
+import Table from '../components/SearchTable';
 
- import Table from '../components/SearchTable';
 
 
- 
 
-function Searchbar () {
-    const [show, setShow] = useState(false);
-    const [APIData, setAPIData] = useState([]);
-    const [filteredResults, setFilteredResults] = useState([]);
-    const [searchInput, setsearchInput] = useState('');
+
+function Searchbar() {
+  const [show, setShow] = useState(false);
+  const [APIData, setAPIData] = useState([]);
+  const [filteredResults, setFilteredResults] = useState([]);
+  const [searchInput, setsearchInput] = useState('');
+  const [owner, setOwner] = useState('');
+const [theme, setTheme] = useState('');
+const [subtheme, setSubtheme] = useState('');
+const [answer, setAnswer] = useState('');
 
   const url = '';
-    useEffect(() => {
-      axios.get(url)
+  useEffect(() => {
+    axios.get(url)
       .then((response) => {
         console.log(response);
         setAPIData(response.data);
-        
+
       })
-    }, [])
+  }, [])
 
 
-    const searchItems = (searchValue) => {
-      setsearchInput(searchValue)
-      if(searchInput !== ''){
-        const filteredData = APIData.filter((item) => {
-          return Object.values(item).join('').toLowerCase().includes(searchInput.toLowerCase())
-        })
-        setFilteredResults(filteredData);
-      }
-      else {
-        setFilteredResults(APIData)
-      }
+  const searchItems = (searchValue) => {
+    setsearchInput(searchValue)
+    if (searchInput !== '') {
+      const filteredData = APIData.filter((item) => {
+        return Object.values(item).join('').toLowerCase().includes(searchInput.toLowerCase())
+      })
+      setFilteredResults(filteredData);
     }
-
-
-
-
-
-    function toggle(){
-      setShow(!show);
+    else {
+      setFilteredResults(APIData)
     }
+  }
 
-    function toggleButton(){
-      setShow(!show);
-    }
+  const answerUrl= 'http://localhost:3000/answers';
+  const addAnswer= async (theme,subtheme,owner,answer) =>{
+    // console.log(owner);
+    //const i=1;
+    const id=Math.floor(Math.random()*1000)+1;
+     const response = await axios.post(answerUrl,{id,...theme,...subtheme,...owner,...answer}).then(()=>{
+       console.log(response);
+     });
+ }
+ const answerSubmit=(e) => {
+  e.preventDefault();
 
-    const handleClick = (event) => {
-        swal({
-            title: "Data submitted successfully!",
-            text: "",
-            icon: "success",
-            buttons: {
-              confirm: true,
-            },
-          });
-      };
-     
+  if(!theme){
+    alert("Please Add Theme and Subtheme");
+    return
+  }
 
-    return (
+  addAnswer({theme,subtheme,owner,answer});
+  setTheme('');
+  setSubtheme('');
+  setOwner('');
+  setAnswer('');
+}
 
-      <>
+
+
+  function toggle() {
+    setShow(!show);
+  }
+
+  function toggleButton() {
+    setShow(!show);
+  }
+
+  const handleClick = (event) => {
+    swal({
+      title: "Data submitted successfully!",
+      text: "",
+      icon: "success",
+      buttons: {
+        confirm: true,
+      },
+    });
+  };
+
+
+  return (
+
+    <>
       {/* Modal */}
-<div className="modal fade " id="exampleModal"  aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div  className="modal-dialog ">
-    <div  className="modal-content position-absolute top-50 start-50 translate-middle" id={styles.modalPosition}>
-      <div  className="modal-header">
-        <h5 className="modal-title" id={styles.titleAlign}>Add Data</h5>
-        <button type="button"  className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      <div className="modal fade " id="exampleModal" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal-dialog ">
+          <div className="modal-content position-absolute top-50 start-50 translate-middle" id={styles.modalPosition}>
+            <div className="modal-header">
+              <h5 className="modal-title" id={styles.titleAlign}>Add Data</h5>
+              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div className="modal-body form-center">
+              <form onSubmit={ answerSubmit}>
+                <select defaultValue={'DEFAULT'} className="form-select mt-3" value={theme} onChange={(e) => setTheme(e.target.value)} aria-label="Default select example">
+                  <option value="DEFAULT">--Theme---</option>
+                  {
+                    data.themes.map((result) => (<option key={result.id}>{result.theme}</option>))
+                  }
+                </select>
+
+                <select defaultValue={'DEFAULT'} className="form-select mt-3" value={subtheme} onChange={(e) => setSubtheme(e.target.value)} aria-label="Default select example">
+                  <option value="DEFAULT">--SubTheme---</option>
+                  {
+                    data.subthemes.map((result) => (<option key={result.id}>{result.subtheme}</option>))
+                  }
+                </select>
+
+                <select defaultValue={'DEFAULT'} className="form-select mt-3" value={owner} onChange={(e) => setOwner(e.target.value)} aria-label="Default select example">
+                  <option value="DEFAULT">--Owner---</option>
+                  {
+                    data.owners.map((result) => (<option key={result.id}>{result.owner}</option>))
+                  }
+                </select>
+                <textarea className="form-control mt-3" id="exampleFormControlTextarea1" rows="3" placeholder="Answer" value={answer} onChange={(e) => setAnswer(e.target.value)}></textarea>
+
+                <div className="modal-footer">
+              <button type="submit" className={styles.saveButton} onClick={handleClick}>Save Data</button>
+            </div>
+              </form>
+            </div>
+          
+          </div>
+        </div>
       </div>
-      <div  className="modal-body form-center">
-      <form>
-      <select defaultValue={'DEFAULT'} className="form-select mt-3" aria-label="Default select example">
-  <option value="DEFAULT">Theme</option>
-  <option value="1">One</option>
-  <option value="2">Two</option>
-  <option value="3">Three</option>
-</select>
-
-<select defaultValue={'DEFAULT'} className="form-select mt-3" aria-label="Default select example">
-  <option value="DEFAULT">Sub Theme</option>
-  <option value="1">One</option>
-  <option value="2">Two</option>
-  <option value="3">Three</option>
-</select>
-
-<input type="email" className="form-control mt-3" id="exampleFormControlInput1" placeholder="Owner"></input>
-<textarea className="form-control mt-3" id="exampleFormControlTextarea1" rows="3"  placeholder="Answer"></textarea>
-      </form>
-      </div>
-      <div  className="modal-footer">
-        <button type="button"  className={styles.saveButton} onClick={handleClick}>Save Data</button>
-      </div>
-    </div>
-  </div>
-</div>
 
 
-        <div className="container" >
-       <div className="row" id={styles.searchBar}>
+      <div className="container" >
+        <div className="row" id={styles.searchBar}>
           <div className="col-sm-12 col-md-8 col-lg-8 p-3"  >
             <input
               type="text"
@@ -116,27 +150,27 @@ function Searchbar () {
           <div className="col-sm-12 col-md-4 col-lg-4 pt-3 ">
             <button type="button" className="btn  pt-2 me-2" id={styles.searchButton} onClick={toggle}>search</button>
             <button type="button" className="btn btn-primary pt-2  me-2"
-             data-bs-toggle="modal" data-bs-target="#exampleModal"
+              data-bs-toggle="modal" data-bs-target="#exampleModal"
             >Add</button>
             <button type="button" className="btn btn-danger pt-2 ">Export</button>
           </div>
+        </div>
+
+        <div className="row">
+          <div className="col-sm-12 col-md-12 col-lg-12 pt-3 " style={{
+            display: show ? "block" : "none"
+          }}>
+            <Table />
           </div>
-
-           <div className="row">
-           <div className="col-sm-12 col-md-12 col-lg-12 pt-3 " style={{
-        display: show?"block":"none"
-      }}>
-           <Table/>
-           </div>
-          </div>
-          
+        </div>
 
 
-       </div>
-     
-      
-       </>
-    );
+
+      </div>
+
+
+    </>
+  );
 
 }
 
